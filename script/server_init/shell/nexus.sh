22760a4814bf09bc5cdc3bce743f29230198fff2
wengyingjian <q291611265@gmail.com>
2016-01-30 13:14:36 +0800

    fix nexus wget

--- script/server_init/shell/nexus.sh
+++ script/server_init/shell/nexus.sh
@@ -2,12 +2,12 @@
 
 TMP_DIR="/tmp"
 NEXUS_URL="https://sonatype-download.global.ssl.fastly.net/nexus/oss/nexus-2.12.0-01-bundle.tar.gz"
-FILE_NAME="neuxs"
+FILE_NAME="nexus"
 INSTALL_DIR="/usr/local"
 
 
 cd ${TMP_DIR}
-wget -O ${FILE_NAME} ${FILE_NAME}.tar.gz
+wget -O ${FILE_NAME}.tar.gz ${NEXUS_URL}
 tar -zxvf ${FILE_NAME}.tar.gz -C ${FILE_NAME}
 mv ${FILE_NAME} ${INTALL_DIR}/${FILE_NAME}
 
